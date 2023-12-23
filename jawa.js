3daca31188b5864d4f9ab9d80ee97a37ad86dee9
Ardianrangga <137834888+UjangRonda@users.noreply.github.com>
2023-12-23 12:01:41 +0700

    Mendandani dashboard / Index

--- jawa.js
+++ jawa.js
@@ -4,3 +4,18 @@ const menuNav = document.querySelector(".menu-navigation");
 menuBar.addEventListener("click", function () {
     menuNav.classList.toggle("menu-active")
 });
+
+function changebg(){
+    var navbars = document.getElementById('navbar')
+    var scrollValue = window.scrollY;
+    if (scrollValue < 80) {
+        navbars.style.transform = "translateY(-100px)";
+        document.getElementById("logo-navbarfloat").style.opacity = "0";
+    }else{
+        console.log(scrollValue)
+        navbars.style.transform = "translateY(0px)";
+        document.getElementById("logo-navbarfloat").style.opacity = "1";
+    }
+}
+
+window.addEventListener('scroll', changebg);
